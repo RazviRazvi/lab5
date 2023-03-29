d3a7f238ffb690e75c94daa2f51aba411a317aa4
RazviRazvi <rgaleseanu@yahoo.com>
2023-03-29 14:15:40 +0300

    1

---
+++ stive.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int Data;
+struct Elem{
+Data val;
+struct Elem* next;
+};
+typedef struct Elem Node;
+
+Data top(Node *top);
+void push(Node**top, Data v); 
+Data pop(Node**top);
+int isEmpty(Node*top);
+void deleteStack(Node**top);
